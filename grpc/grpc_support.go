350f65f22bb381449e3f727fce1760140c51e05f
tckz <at.tckz@gmail.com>
2021-10-07 18:44:42 +0900

    自module内もimportも新modに書き換え

--- grpc/grpc_support.go
+++ grpc/grpc_support.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"net/http"
 
-	"github.com/tckz/vegetahelper"
+	vegetahelper "github.com/tckz/vegetahelpermod"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
